e534a060a0565bd7f19553c43fc2910089eda062
cantzuo <156641993+cantzuo@users.noreply.github.com>
2025-05-03 17:39:53 +0800

    更新 cf.sh

--- cf/cf.sh
+++ cf/cf.sh
@@ -52,7 +52,9 @@ cleanup() {
 
 # 下载必要文件
 download_files() {
-    local base_url="https://raw.githubusercontent.com/yonggekkk/Cloudflare_vless_trojan/refs/heads/main/cf"
+    local base_url="https://raw.githubusercontent
+.com/cantzuo/yg_vless_trojan/refs/heads/main/cf/cf.s
+h"
     
     # 使用更可靠的下载方式
     if ! curl -L -o cf --connect-timeout 15 --retry 3 "$base_url/$cpu"; then
