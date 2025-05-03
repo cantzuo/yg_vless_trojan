af2be943651f0d0fcd373639ecaac7f390bbc537
cantzuo <156641993+cantzuo@users.noreply.github.com>
2025-05-03 17:52:54 +0800

    更新 cf.sh

--- cf/cf.sh
+++ cf/cf.sh
@@ -52,8 +52,7 @@ cleanup() {
 
 # 下载必要文件
 download_files() {
-    local base_url="https://ghfast.top/https://raw.githubusercontent
-.com/cantzuo/yg_vless_trojan/refs/heads/main/cf"
+    local base_url="https://raw.githubusercontent.com/cantzuo/yg_vless_trojan/refs/heads/main/cf"
     
     # 使用更可靠的下载方式
     if ! curl -L -o cf --connect-timeout 15 --retry 3 "$base_url/$cpu"; then
